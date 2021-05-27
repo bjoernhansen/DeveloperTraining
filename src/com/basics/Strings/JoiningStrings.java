cb3a6d1c7bcfa4d5f2bee7a91f79a338a3fa73a6
bjoern.hansen <bjoern.hansen@mspag.com>
2021-05-27 22:44:28 +0200

    added examples for some string operations

---
+++ src/com/basics/Strings/JoiningStrings.java
@@ -0,0 +1,18 @@
+package com.basics.Strings;
+
+import java.util.StringJoiner;
+
+public class JoiningStrings {
+
+    public static void main(String[] args) {
+        StringJoiner joiner = new StringJoiner(",");
+        joiner.add("01")
+              .add("02")
+              .add("03");
+        String joinedString = joiner.toString(); // "01,02,03"
+        String joinedString2 = String.join(" - ", "04", "05", "06"); // "04 - 05 - 06"
+
+
+        StringBuilder s;
+    }
+}
