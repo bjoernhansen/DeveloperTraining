b2460a3ee7e7ab5f4208cf0cd19508bf3fbba7d0
bjoernhansen <hansen_bjoern@web.de>
2022-04-22 16:25:58 +0200

    added source code testing threads

---
+++ src/com/concurrency/threads/Anonym.java
@@ -0,0 +1,22 @@
+package com.concurrency.threads;
+
+
+public class Anonym {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
+		Thread t1 = new Thread(new Runnable()
+		{			
+			@Override
+			public void run()
+			{				
+				
+			}
+		});
+		
+		// Lambda 
+		Thread t2 = new Thread(()->{});
+		t2.start();	 // Fehlerquelle: vergessen zu starten
+	}
+}
