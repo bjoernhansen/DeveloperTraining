e0e7ab92acb432f4a9ec633667cb1e5c78ac6c76
bjoernhansen <hansen_bjoern@web.de>
2023-10-05 09:20:47 +0200

    updated class JavaDocExample

--- src/main/java/training/basics/javadocs/JavaDocExample.java
+++ src/main/java/training/basics/javadocs/JavaDocExample.java
@@ -19,4 +19,24 @@ public class JavaDocExample
         throw new RuntimeException();
     }
 
+    /** Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #main}.
+     */
+    void method()
+    {
+        
+    }
+
+    /**
+     * Creates a {@code String} instance which is a concatenation of the input parameters
+     * @param text1 first text to be concatenated with {@code text2}
+     * @param text2 suffix of the returned String instance
+     * @return a concatenation of text1 and text2
+     */
+    String concat(String text1, String text2)
+    {
+        return text1 + text2;
+    }
+
 }
